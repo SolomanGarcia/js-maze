46edee8a08de1bb7db988ee842eb845ffd7dbb31
Soloman Garcia <soloman.garcia@gmail.com>
2020-06-18 15:43:05 -0600

    work on stepThroughCell function

--- index.js
+++ index.js
@@ -45,8 +45,20 @@ const startColumn = Math.floor(Math.random() * cells);
 
 const stepThroughCell = (row, column) => {
   // If I have visited the cell at [row, column], then return
+  if (grid[row][column]) {
+    return;
+  }
+
   // Mark this cell as being visited
+  grid([row][column]) = true;
+
   // Assemble randomly-ordered list of neighbors
+  const neighbors = [
+    [row - 1, column],
+    [row, column + 1],
+    [row + 1, column],
+    [row, column - 1]
+  ];
   // For each neighbor....
   // See if that neighbor is out of bounds
   // If we have visited that neighbor, continue to next neighbor
