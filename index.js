9d9bfa33f5ffdc5c10ab81d548fc71db43c3aad9
Soloman Garcia <soloman.garcia@gmail.com>
2020-06-18 12:33:42 -0600

    generate random shapes

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const {
   World,
   Bodies,
   MouseConstraint,
-  Mouse,
+  Mouse
 } = Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -14,9 +17,10 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600,
-  },
+    wireframes: false,
+    width,
+    height
+  }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
@@ -24,7 +28,7 @@ Runner.run(Runner.create(), engine);
 World.add(
   world,
   MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas),
+    mouse: Mouse.create(render.canvas)
   })
 );
 
@@ -33,8 +37,25 @@ const walls = [
   Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
   Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
   Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
 ];
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// Random Shapes
+for (let i = 0; i < 50; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  } else {
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+        render: {
+          fillStyle: "tomato"
+        }
+      })
+    );
+  }
+}
