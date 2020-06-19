4559a1f731278557da23779100a00c1f1645e2f5
Soloman Garcia <soloman.garcia@gmail.com>
2020-06-19 08:14:38 -0600

    update vertical wall values

--- index.js
+++ index.js
@@ -97,6 +97,11 @@ const stepThroughCell = (row, column) => {
     }
 
     // Remove a wall from either horizontals or verticals
+    if (direction === "left") {
+      verticals[row][column - 1] = true;
+    } else if (direction === "right") {
+      verticals[row][column] = true;
+    }
   }
 
   // Visit the next cell
